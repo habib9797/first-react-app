91ee9b86e910d31bddb03662001a80c1f3327b6d
habib9797 <kaxa97kz@mail.ru>
2024-07-17 21:16:58 +0600

    DemonSlayer

---
+++ src/components/DemonSlayer.jsx
@@ -0,0 +1,10 @@
+function DemonSlayer({ name = 'Gyomei', target = 'Kukushibo' }) {
+	return (
+		<>
+			<h1>
+				Один из лучших столпов {name} убивает демона старшей луны {target}
+			</h1>
+		</>
+	)
+}
+export default DemonSlayer
